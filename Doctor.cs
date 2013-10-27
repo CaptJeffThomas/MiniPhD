b00eaf0621bdf73b12dd2df1011762b8480e1cf7
Jeff Thomas <jthomasesq@gmail.com>
2013-10-27 05:03:54 -0600

    First Commit with base Virus/Doctor classes

---
+++ Doctor.cs
@@ -0,0 +1,19 @@
+/// Mini PhD - 2013 ///
+/// by  Jeff Thomas, Nico Walker, Tyler Bach ///
+
+/// This is our player-controlled Doctor and all of his related attributes///
+
+
+
+public class Doctor
+{
+
+	public double x, y;        //coordinates of Doctor sprite
+
+	
+	//Do we need to have movement and sprite update functions here or is that done in a specific Unity function?
+	//No idea what else Doctor class has to hold XD
+
+
+
+}
