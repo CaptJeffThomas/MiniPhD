2014777bb11718fa1809ff1a8c2df3cfd9bb6309
Jeff Thomas <jthomasesq@gmail.com>
2013-11-22 13:31:16 -0700

    Proper Virus class.

--- Project/Assets/Scripts/Virus.cs
+++ Project/Assets/Scripts/Virus.cs
@@ -1,45 +1,172 @@
-﻿using UnityEngine;
-using System.Collections;
+/// Mini PhD - 2013 ///
+/// by  Jeff Thomas, Nico Walker, Tyler Bach ///
 
-public class Virus : MonoBehaviour {
+/// These classes define the different virii our intrepid Doctor must defeat ///
+
+const int MELEE = 1;
+const int RANGED = 2;
+
+//Virus is the superclass that all our specific Virii are formed from.
+public abstract class Virus
+{
+	public double x, y;        //coordinates of virus sprite
+	public int hp; //meleeDef, rangeDef;  I don't think we need to implement these Def stats after further consideration
 	
-	public float x, y;
-	public float movSpeedX, movSpeedY;
+	public double lifeTime;  //time virus has been alive.
+	public double spreadThreshold; //time after spawning until virii spreads.  Do we want this to vary between colors?
 	
-	public float elapsedTime; //continuous counter that is incremented each frame - for timed actions
-	
-	// Use this for initialization
-	void Start () {
-		
-		x = 0.0f;
-		y = 0.0f;
-		movSpeedX = 0.0f;
-		movSpeedY = 0.0f;
-		
+    public Virus()
+	{
 	}
 	
-	// Update is called once per frame
-	void Update () {
+
+	public takeDamage(int dmgType, int dmg){
+	//dmgType tells us if the dmg is melee/ranged, and dmg is the base damage from weapon.  
+	//ATM dmg is always 1 but it could change in future if we want upgrades or something
+	}
+	
+	
+	//Upon reaching 0 hp, the virus's sprite and stored object needs to be cleaned up
+	public virusDeath(){
+	}
+	
+	
+	//once the individual virus have passed a certain lifeTime threshold, it will spread and spawn new virii.
+	public spread(){
+	}
+
+}
+
+
+
+//These Virii are resistant to range damage.
+public class BlueVirus : Virus
+{
+    public BlueVirus(double xPos, double yPos)
+    {
+        x = xPos;
+		y = yPos;
 		
-		elapsedTime += Time.deltaTime;
+		hp = 3;
+		//meleeDef = 0;
+		//rangeDef = 1;
 		
-		if(elapsedTime > 0.5f)
-		{
-			movSpeedX = Random.Range(-20.0f, 20.0f);
-			movSpeedY = Random.Range(-20.0f, 20.0f);
-			elapsedTime -= 0.5f;
+		lifeTime = 0.0;
+		spreadThreshold = 20;
+    }
+	
+	
+	public takeDamage(int dmgType, int dmg){
+		
+		if (dmgType == MELEE){
+			hp = hp - dmg;
 		}
 		
-		
-		/*if(movSpeedX > 0.0f)
-			transform.Translate(Vector3.right * movSpeedX * Time.deltaTime);
-		else
-			transform.Translate(Vector3.left * movSpeedX * Time.deltaTime);*/
-		
-		x += (movSpeedX * Time.deltaTime);
-		y += (movSpeedY * Time.deltaTime);
-		
-		transform.Translate(x, y, 0.0f);
+		if (hp <= 0){
+			virusDeath();
+		}
 		
 	}
+	
+	public virusDeath(){
+	
+	}
+	
+	public spread(){
+		
+		if(lifeTime >= spreadThreshold){
+			//spawn new BlueVirus adjacent
+		}
+		
+	}
+}
+
+
+
+//These Virii have no increased resistance, but have large HP pools.  Spreads faster than others.
+public class GreenVirus : Virus
+{
+    public GreenVirus(double xPos, double yPos)
+    {
+        x = xPos;
+		y = yPos;
+		
+		hp = 6;
+		//meleeDef = 0;
+		//rangeDef = 0;
+		
+		lifetime = 0.0;
+		spreadThreshold = 12;
+    }
+	
+	
+	public takeDamage(int dmgType, int dmg){
+		
+		hp = hp - dmg;
+		
+		if (hp <= 0){
+			virusDeath();
+		}
+	}
+	
+	
+	public virusDeath(){
+	
+	}
+	
+	
+	public spread(){
+		
+		if(lifeTime >= spreadThreshold){
+			//spawn new GreenVirus adjacent
+		}
+		
+	}        
+   
+}
+
+
+
+//These Virii are resistant to melee damage.
+public class RedVirus : Virus
+{
+    public RedVirus(double xPos, double yPos)
+    {
+        x = xPos;
+		y = yPos;
+		
+		hp = 3;
+		//meleeDef = 0;
+		//rangeDef = 0;
+		
+		lifetime = 0.0;
+		spreadThreshold = 20;
+    }
+	
+	
+	public takeDamage(int dmgType, int dmg){
+
+		if (dmgType == RANGED){
+			hp = hp - dmg;
+		}
+		
+		if (hp <= 0){
+			virusDeath();
+		}
+	}
+	
+	
+	public virusDeath(){
+	
+	}
+	
+	
+	public spread(){
+		
+		if(lifeTime >= spreadThreshold){
+			//spawn new GreenVirus adjacent
+		}
+		
+	}        
+   
 }
